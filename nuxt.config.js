b7494852350323fe7f7e97957b1dd2b2dcf2b140
Senjin Hajrulahovic <senjin.hajrulahovic@gmail.com>
2022-09-02 20:30:47 +0200

    create distdir

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,13 +33,14 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    // '@nuxt/typescript-build'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
+    '@nuxt/content'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
